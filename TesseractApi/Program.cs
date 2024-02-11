b1466b2e011223d5ca18e39e7479a39264f7cbdc
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2024-02-11 17:14:15 -0300

    Removendo Controller e adotando Minimal API's

--- TesseractApi/Program.cs
+++ TesseractApi/Program.cs
@@ -1,29 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using TesseractApi;
 using TesseractApi.Services;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<TesseractService>();
 
-builder.Services.AddControllers();
-
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-
-app.UseRouting();
-
-app.MapControllers();
+app.MapTesseractEndpoints();
 
 app.Run();
+
